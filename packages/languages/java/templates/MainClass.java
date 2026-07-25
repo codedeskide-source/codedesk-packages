cc16e8522cb21571a501e0c5343f9a2961ae28e5
codedeskide-source <code.desk.ide@gmail.com>
2026-07-25 13:22:17 -0500

    Add MainClass template for Java projects

---
+++ packages/languages/java/templates/MainClass.java
@@ -0,0 +1,12 @@
+${packageDeclaration}
+
+public final class ${ClassName} {
+
+    private ${ClassName}() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Code Desk Java project started.");
+    }
+
+}
